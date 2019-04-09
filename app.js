692ef61e4d269ae053483417b23f7a0821e0fd1d
Sibasish Subhadarshee <deadpool0247ishit@gmail.com>
2019-04-10 00:19:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,11 +95,7 @@ app.get('/connect/callback', function(req, res) {
                     });
                     console.log("Three")
                     newUser.save()
-                    .then( () => res.status(200).send("Your account was successfuly authenticated"))
-                    .catch((err) => {
-                        console.log('error in newuser save of connectcallback');
-                        res.status(400).json({error:err});
-                    })
+                
                     console.log("Four")
                 }
             });
